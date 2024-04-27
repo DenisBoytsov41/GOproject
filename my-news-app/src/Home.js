fc2573b0e26b062282b4ee0b7e391e4a74048dbc
lab1_web <goodpromised41@gmail.com>
2024-04-27 18:27:02 +0300

    fix

--- my-news-app/src/Home.js
+++ my-news-app/src/Home.js
@@ -1,229 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './assets/style.css';
 import './assets/button.css';
-
-const cacheExpirationTime = 60 * 60 * 1000;
+import useNewsSearch from './useNewsSearch';
 
 function Home() {
-  const [searchKey, setSearchKey] = useState(localStorage.getItem('searchKey') || '');
-  const [results, setResults] = useState('');
-  const [currentPage, setCurrentPage] = useState(parseInt(localStorage.getItem('currentPage'), 10) || 1);
-  const [totalPages, setTotalPages] = useState(1);
-  const [visiblePagesCount] = useState(10);
-  const [visiblePages, setVisiblePages] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const startPage = Math.max(1, currentPage - Math.floor(visiblePagesCount / 2));
-    const endPage = Math.min(totalPages, startPage + visiblePagesCount - 1);
-
-    const pages = Array.from({ length: endPage - startPage + 1 }, (_, index) => index + startPage);
-    setVisiblePages(pages);
-    localStorage.setItem('currentPage', currentPage);
-  }, [currentPage, totalPages, visiblePagesCount]);
-
-  useEffect(() => {
-    localStorage.setItem('searchKey', searchKey);
-  }, [searchKey]);
-
-  useEffect(() => {
-    localStorage.setItem('currentPage', currentPage);
-  }, [currentPage]);
-
-  useEffect(() => {
-    setCurrentPage(1);
-    setResults('');
-  }, [searchKey]);
-
-  const fetchData = async (url) => {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error('Ошибка при получении данных');
-    }
-    return response.json();
-  };
-
-  const delayedFetch = async (url, delay) => {
-    return new Promise((resolve) => {
-      setTimeout(async () => {
-        resolve(await fetchData(url));
-      }, delay);
-    });
-  };
-
-  useEffect(() => {
-    if (results) {
-      setLoading(false);
-      console.log(results);
-      if (currentPage===1)
-      {
-        try {
-          if (results.totalResults >0)
-          {
-            console.log("Я тут");
-            setTotalPages(Math.ceil(results.totalResults / 20));
-          }
-        }
-        catch (error) {
-          if (results.Results.totalResults >0)
-          {
-            console.log("Я тут");
-            setTotalPages(Math.ceil(results.Results.totalResults / 20));
-          }
-        } 
-        
-      }
-      else
-        setTotalPages(Math.ceil(results.Results.totalResults / 20));
-      console.log(currentPage);
-      console.log(totalPages);
-      console.log(!loading);
-
-    }
-  }, [results]);
-
-  const handleSearch = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    try {
-      const cachedResults = JSON.parse(localStorage.getItem(`${searchKey}-page-1`));
-      console.log(cachedResults);
-      if (cachedResults && Date.now() - cachedResults.timestamp < cacheExpirationTime && cachedResults.totalResults != null) {
-        setResults(cachedResults);
-        setCurrentPage(1);
-        setLoading(false);
-        return;
-      }
-  
-      const response = await fetch(`http://localhost:5000/search?q=${searchKey}&page=1`);
-      if (!response.ok) {
-        throw new Error('Ошибка при получении данных');
-      }
-      const data = await delayedFetch(`http://localhost:5000/search?q=${searchKey}&page=1`, 1000);
-      console.log(data);
-      setResults(data);
-      setCurrentPage(1);
-      setTotalPages(Math.ceil(data.totalResults / 20));
-      localStorage.setItem(`${searchKey}-page-${currentPage}`, JSON.stringify({ data, timestamp: Date.now() }));
-    } catch (error) {
-      console.error('Ошибка:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
-  
-  const handleNextPage = async () => {
-    if (currentPage < totalPages) {
-      setLoading(true);
-      try {
-        const nextPage = currentPage + 1;
-        const cachedResults = JSON.parse(localStorage.getItem(`${searchKey}-page-${nextPage}`));
-        console.log(cachedResults);
-        console.log(cachedResults.data.Results);
-        if (cachedResults && Date.now() - cachedResults.timestamp < cacheExpirationTime && cachedResults.data.Results.totalResults != null) {
-          setCurrentPage(nextPage);
-          setResults(cachedResults);
-          console.log("Я тут2");
-          console.log(currentPage);
-          setLoading(false);
-          return;
-        }
-        const data = await delayedFetch(`http://localhost:5000/search?q=${searchKey}&page=${nextPage}`, 1000);
-        setResults(data);
-        setCurrentPage(nextPage);
-        setTotalPages(Math.ceil(data.Results.totalResults / 20));
-        localStorage.setItem(`${searchKey}-page-${nextPage}`, JSON.stringify({ data, timestamp: Date.now() }));
-      } catch (error) {
-        console.error('Ошибка:', error);
-      } finally {
-        setLoading(false);
-      }
-    }
-  };
-
-  const handlePreviousPage = async () => {
-    if (currentPage > 1) {
-      setLoading(true);
-      try {
-        const prevPage = currentPage - 1;
-        const cachedResults = JSON.parse(localStorage.getItem(`${searchKey}-page-${prevPage}`));
-        console.log(cachedResults);
-        if (prevPage===1)
-          if (cachedResults && Date.now() - cachedResults.timestamp < cacheExpirationTime && cachedResults.totalResults != null) {
-            setResults(cachedResults);
-            setCurrentPage(prevPage);
-            setLoading(false);
-            return;
-          }
-        else
-          if (cachedResults && Date.now() - cachedResults.timestamp < cacheExpirationTime && cachedResults.data.Results.totalResults != null) {
-            setResults(cachedResults);
-            setCurrentPage(prevPage);
-            setLoading(false);
-            return;
-          }
-        const data = await delayedFetch(`http://localhost:5000/search?q=${searchKey}&page=${prevPage}`, 1000);
-        setResults(data);
-        setCurrentPage(prevPage);
-        console.log(prevPage);
-        if (prevPage===1)
-        {
-          console.log(prevPage===1);
-          setTotalPages(Math.ceil(results.totalResults / 20));
-        }
-        else
-        {
-          console.log(prevPage===1);
-          console.log(results);
-          console.log(data);
-          console.log(Math.ceil(results.Results.totalResults / 20));
-          setTotalPages(Math.ceil(results.Results.totalResults / 20));
-        }
-        localStorage.setItem(`${searchKey}-page-${prevPage}`, JSON.stringify({ data, timestamp: Date.now() }));
-      } catch (error) {
-        console.error('Ошибка:', error);
-      } finally {
-        setLoading(false);
-      }
-    }
-  };
-
-  const handlePageClick = async (page) => {
-    setLoading(true);
-    try {
-      const cachedResults = JSON.parse(localStorage.getItem(`${searchKey}-page-${page}`));
-      if (page===1)
-        if (cachedResults && Date.now() - cachedResults.timestamp < cacheExpirationTime && cachedResults.totalResults != null) {
-          setResults(cachedResults);
-          setCurrentPage(page);
-          setLoading(false);
-          return;
-        }
-      else
-        if (cachedResults && Date.now() - cachedResults.timestamp < cacheExpirationTime && cachedResults.data.Results.totalResults != null) {
-          setResults(cachedResults);
-          setCurrentPage(page);
-          setLoading(false);
-          return;
-        }
-      const data = await delayedFetch(`http://localhost:5000/search?q=${searchKey}&page=${page}`, 1000);
-      setResults(data);
-      setCurrentPage(page);
-      if (page===1)
-      {
-        setTotalPages(Math.ceil(results.totalResults / 20));
-      }
-      else
-      {
-        setTotalPages(Math.ceil(results.Results.totalResults / 20));
-      }
-      localStorage.setItem(`${searchKey}-page-${page}`, JSON.stringify({ data, timestamp: Date.now() }));
-    } catch (error) {
-      console.error('Ошибка:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const { searchKey, setSearchKey, results, currentPage, totalPages, loading, visiblePages, handleSearch, handleNextPage, handlePreviousPage, handlePageClick } = useNewsSearch();
 
   return (
     <div className="App">
@@ -246,65 +27,95 @@ function Home() {
         {loading && <p>Загрузка...</p>}
         {!loading && results ? (
           <div className="result-count">
-            {currentPage === 1 ? (
-              results.totalResults > 0 ? (
-                <p>Найдено <strong>{results.totalResults}</strong> новостей. Вы на странице <strong>{currentPage}</strong> из <strong>{totalPages}</strong>.</p>
-              ) : (
-                <p>Новости по запросу "<strong>{searchKey}</strong>" не найдены.</p>
-              )
-            ) : (
-              results.Results.totalResults > 0 ? (
-                <p>Найдено <strong>{results.Results.totalResults}</strong> новостей. Вы на странице <strong>{currentPage}</strong> из <strong>{totalPages}</strong>.</p>
-              ) : (
-                <p>Новости по запросу "<strong>{searchKey}</strong>" не найдены.</p>
-              )
-            )}
-          </div>
+          {(() => {
+            try {
+              if (results.totalResults > 0) {
+                return (
+                  <p>Найдено <strong>{results.totalResults}</strong> новостей. Вы на странице <strong>{currentPage}</strong> из <strong>{totalPages}</strong>.</p>
+                );
+              } else {
+                return (
+                  <p>Новости по запросу "<strong>{searchKey}</strong>" не найдены.</p>
+                );
+              }
+            } catch (error) {
+              try {
+                if (results.Results.totalResults > 0) {
+                  return (
+                    <p>Найдено <strong>{results.Results.totalResults}</strong> новостей. Вы на странице <strong>{currentPage}</strong> из <strong>{totalPages}</strong>.</p>
+                  );
+                } else {
+                  return (
+                    <p>Новости по запросу "<strong>{searchKey}</strong>" не найдены.</p>
+                  );
+                }
+              } catch (error) {
+                console.error('Ошибка при рендеринге результатов поиска:', error);
+                return null;
+              }
+            }
+          })()}
+        </div>        
         ) : null}
         {!loading && results && (
-          currentPage === 1 ? (
-            results.articles && Array.isArray(results.articles) && results.articles.length > 0 ? (
-              <ul className="search-results">
-                {results.articles.map((article, index) => (
-                  <li key={index} className="news-article">
-                    <div>
-                      <a target="_blank" rel="noreferrer noopener" href={article.url}>
-                        <h3 className="title">{article.title}</h3>
-                      </a>
-                      <p className="description">{article.description}</p>
-                      <div className="metadata">
-                        <p className="source">{article.source.name}</p>
-                        <time className="published-date">{article.publishedAt}</time>
-                      </div>
-                    </div>
-                    <img className="article-image" src={article.urlToImage} alt={article.title} />
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>Новости по запросу "<strong>{searchKey}</strong>" не найдены.</p>
-            )
-          ) : (
-            results.Results && results.Results.articles && Array.isArray(results.Results.articles) && results.Results.articles.length > 0 ? (
-              <ul className="search-results">
-                {results.Results.articles.map((article, index) => (
-                  <li key={index} className="news-article">
-                    <div>
-                      <a target="_blank" rel="noreferrer noopener" href={article.url}>
-                        <h3 className="title">{article.title}</h3>
-                      </a>
-                      <p className="description">{article.description}</p>
-                      <div className="metadata">
-                        <p className="source">{article.source.name}</p>
-                        <time className="published-date">{article.publishedAt}</time>
-                      </div>
-                    </div>
-                    <img className="article-image" src={article.urlToImage} alt={article.title} />
-                  </li>
-                ))}
-              </ul>
-            ) : null
-          )
+          (() => {
+            try {
+              if (results.articles && Array.isArray(results.articles) && results.articles.length > 0) {
+                return (
+                  <ul className="search-results">
+                    {results.articles.map((article, index) => (
+                      <li key={index} className="news-article">
+                        <div>
+                          <a target="_blank" rel="noreferrer noopener" href={article.url}>
+                            <h3 className="title">{article.title}</h3>
+                          </a>
+                          <p className="description">{article.description}</p>
+                          <div className="metadata">
+                            <p className="source">{article.source.name}</p>
+                            <time className="published-date">{article.publishedAt}</time>
+                          </div>
+                        </div>
+                        <img className="article-image" src={article.urlToImage} alt={article.title} />
+                      </li>
+                    ))}
+                  </ul>
+                );
+              } else {
+                return (
+                  <p>Новости по запросу "<strong>{searchKey}</strong>" не найдены.</p>
+                );
+              }
+            } catch (error) {
+              try {
+                if (results.Results && results.Results.articles && Array.isArray(results.Results.articles) && results.Results.articles.length > 0) {
+                  return (
+                    <ul className="search-results">
+                      {results.Results.articles.map((article, index) => (
+                        <li key={index} className="news-article">
+                          <div>
+                            <a target="_blank" rel="noreferrer noopener" href={article.url}>
+                              <h3 className="title">{article.title}</h3>
+                            </a>
+                            <p className="description">{article.description}</p>
+                            <div className="metadata">
+                              <p className="source">{article.source.name}</p>
+                              <time className="published-date">{article.publishedAt}</time>
+                            </div>
+                          </div>
+                          <img className="article-image" src={article.urlToImage} alt={article.title} />
+                        </li>
+                      ))}
+                    </ul>
+                  );
+                } else {
+                  return null;
+                }
+              } catch (error) {
+                console.error('Ошибка при рендеринге результатов поиска:', error);
+                return null;
+              }
+            }
+          })()
         )}
         {!loading && searchKey && results && totalPages > 0 ? (
           <div className="pagination">
---
+++ my-news-app/src/useNewsSearch.js
@@ -0,0 +1,283 @@
+import { useState, useEffect } from 'react';
+
+const cacheExpirationTime = 60 * 60 * 1000;
+
+const useNewsSearch = () => {
+  const [searchKey, setSearchKey] = useState(localStorage.getItem('searchKey') || '');
+  const [results, setResults] = useState('');
+  const [currentPage, setCurrentPage] = useState(parseInt(localStorage.getItem('currentPage'), 10) || 1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [visiblePagesCount] = useState(10);
+  const [visiblePages, setVisiblePages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const startPage = Math.max(1, currentPage - Math.floor(visiblePagesCount / 2));
+    const endPage = Math.min(totalPages, startPage + visiblePagesCount - 1);
+
+    const pages = Array.from({ length: endPage - startPage + 1 }, (_, index) => index + startPage);
+    setVisiblePages(pages);
+    localStorage.setItem('currentPage', currentPage);
+  }, [currentPage, totalPages, visiblePagesCount]);
+
+  useEffect(() => {
+    localStorage.setItem('searchKey', searchKey);
+  }, [searchKey]);
+
+  useEffect(() => {
+    if (results) {
+      console.log(results);
+      try {
+        console.log("Я тут");
+        setTotalPages(Math.ceil(results.totalResults / 20));
+      }
+      catch (error) {
+        console.log("Я тут");
+        setTotalPages(Math.ceil(results.Results.totalResults / 20));
+      } 
+      console.log(currentPage);
+      console.log(totalPages);
+      console.log(!loading);
+      setLoading(false);
+
+    }
+  }, [results]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const cachedResults = JSON.parse(localStorage.getItem(`${searchKey}-page-1`));
+      try
+      {
+        if (cachedResults && Date.now() - cachedResults.timestamp < cacheExpirationTime && cachedResults.totalResults != null) {
+          setResults(cachedResults);
+          setCurrentPage(1);
+          setLoading(false);
+          return;
+        }
+      }
+      catch(e)
+      {
+        if (cachedResults && Date.now() - cachedResults.timestamp < cacheExpirationTime && cachedResults.data.Results.totalResults != null) {
+          setResults(cachedResults);
+          setCurrentPage(1);
+          setLoading(false);
+          return;
+        }
+      }
+      
+  
+      fetch(`http://localhost:5000/search?q=${searchKey}&page=1`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Ошибка при получении данных');
+          }
+          return response.json();
+        })
+        .then(data => {
+          try
+          {
+            setResults(data);
+            setCurrentPage(1);
+            setTotalPages(Math.ceil(data.totalResults / 20));
+            localStorage.setItem(`${searchKey}-page-${currentPage}`, JSON.stringify({ data, timestamp: Date.now() }));
+          }
+          catch(e)
+          {
+            setResults(data);
+            setCurrentPage(1);
+            setTotalPages(Math.ceil(data.Results.totalResults / 20));
+            localStorage.setItem(`${searchKey}-page-${currentPage}`, JSON.stringify({ data, timestamp: Date.now() }));
+          }
+          
+        })
+        .catch(error => console.error('Ошибка:', error))
+        .finally(() => setLoading(false));
+    } 
+    catch (error) {
+      console.error('Ошибка:', error);
+      setLoading(false);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setLoading(true);
+      try {
+        const nextPage = currentPage + 1;
+        const cachedResults = JSON.parse(localStorage.getItem(`${searchKey}-page-${nextPage}`));
+        try {
+          if (cachedResults && Date.now() - cachedResults.timestamp < cacheExpirationTime && cachedResults.totalResults != null) {
+            setCurrentPage(nextPage);
+            setResults(cachedResults);
+            console.log("daaf");
+            console.log(currentPage);
+            setLoading(false);
+            return;
+          }
+        }
+        catch(e) {
+          if (cachedResults && Date.now() - cachedResults.timestamp < cacheExpirationTime && cachedResults.data.Results.totalResults != null) {
+            setCurrentPage(nextPage);
+            setResults(cachedResults);
+            console.log("daaf");
+            console.log(currentPage);
+            setLoading(false);
+            return;
+          }
+        }
+        fetch(`http://localhost:5000/search?q=${searchKey}&page=${nextPage}`)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Ошибка при получении данных');
+            }
+            return response.json();
+          })
+          .then(data => {
+            try
+            {
+              setResults(data);
+              setCurrentPage(nextPage);
+              console.log(data);
+              setTotalPages(Math.ceil(data.Results.totalResults / 20));
+              localStorage.setItem(`${searchKey}-page-${nextPage}`, JSON.stringify({ data, timestamp: Date.now() }));
+              setLoading(false);
+            }
+            catch(e)
+            {
+              setResults(data);
+              setCurrentPage(nextPage);
+              console.log(data);
+              setTotalPages(Math.ceil(data.totalResults / 20));
+              localStorage.setItem(`${searchKey}-page-${nextPage}`, JSON.stringify({ data, timestamp: Date.now() }));
+              setLoading(false);
+            }
+            
+          })
+          .catch(error => console.error('Ошибка:', error))
+          .finally(() => setLoading(false));
+          setCurrentPage(nextPage);
+          console.log("New currentPage:", nextPage);
+          console.log(currentPage);
+      } catch (error) {
+        console.error('Ошибка:', error);
+        setLoading(false);
+      }
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setLoading(true);
+      try {
+        const prevPage = currentPage - 1;
+        const cachedResults = JSON.parse(localStorage.getItem(`${searchKey}-page-${prevPage}`));
+        try
+        {
+          if (cachedResults && Date.now() - cachedResults.timestamp < cacheExpirationTime && cachedResults.totalResults != null) {
+            setResults(cachedResults);
+            setCurrentPage(prevPage);
+            setLoading(false);
+            return;
+          }
+        }
+        catch(e)
+        {
+          if (cachedResults && Date.now() - cachedResults.timestamp < cacheExpirationTime && cachedResults.data.Results.totalResults != null) {
+            setResults(cachedResults);
+            setCurrentPage(prevPage);
+            setLoading(false);
+            return;
+          }
+        }
+        fetch(`http://localhost:5000/search?q=${searchKey}&page=${prevPage}`)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Ошибка при получении данных');
+            }
+            return response.json();
+          })
+          .then(data => {
+            try
+            {
+              setResults(data);
+              setCurrentPage(prevPage);
+              setTotalPages(Math.ceil(data.totalResults / 20));
+              localStorage.setItem(`${searchKey}-page-${prevPage}`, JSON.stringify({ data, timestamp: Date.now() }));
+              setLoading(false);
+            }
+            catch(e)
+            {
+              setResults(data);
+              setCurrentPage(prevPage);
+              setTotalPages(Math.ceil(data.Results.totalResults / 20));
+              localStorage.setItem(`${searchKey}-page-${prevPage}`, JSON.stringify({ data, timestamp: Date.now() }));
+              setLoading(false);
+            }
+            
+          })
+          .catch(error => console.error('Ошибка:', error))
+          .finally(() => setLoading(false));
+      } catch (error) {
+        console.error('Ошибка:', error);
+        setLoading(false);
+      }
+    }
+  };
+
+  const handlePageClick = (page) => {
+    setLoading(true);
+    try {
+      const cachedResults = JSON.parse(localStorage.getItem(`${searchKey}-page-${page}`));
+      try 
+      {
+        if (cachedResults && Date.now() - cachedResults.timestamp < cacheExpirationTime && cachedResults.totalResults != null) {
+          setResults(cachedResults);
+          setCurrentPage(page);
+          setLoading(false);
+          return;
+        }
+      }
+      catch(e)
+      {
+        if (cachedResults && Date.now() - cachedResults.timestamp < cacheExpirationTime && cachedResults.data.Results.totalResults != null) {
+          setResults(cachedResults);
+          setCurrentPage(page);
+          setLoading(false);
+          return;
+        }
+      }
+      fetch(`http://localhost:5000/search?q=${searchKey}&page=${page}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Ошибка при получении данных');
+          }
+          return response.json();
+        })
+        .then(data => {
+          setResults(data);
+          setCurrentPage(page);
+          try
+          {
+            setTotalPages(Math.ceil(data.totalResults / 20));
+          }
+          catch(e)
+          {
+            setTotalPages(Math.ceil(data.Results.totalResults / 20));
+          }
+          localStorage.setItem(`${searchKey}-page-${page}`, JSON.stringify({ data, timestamp: Date.now() }));
+          setLoading(false);
+        })
+        .catch(error => console.error('Ошибка:', error))
+        .finally(() => setLoading(false));
+    } catch (error) {
+      console.error('Ошибка:', error);
+      setLoading(false);
+    }
+  };
+
+  return { searchKey, setSearchKey, results, currentPage, totalPages, loading, visiblePages, handleSearch, handleNextPage, handlePreviousPage, handlePageClick };
+};
+
+export default useNewsSearch;
